af84ddcee27325ced68cdac84ca78fb85e325a66
sophiagrilli <sophiagrilli@yahoo.com>
2022-08-02 20:35:26 -0300

    cambios

---
+++ listado_productos.php
@@ -0,0 +1,39 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_statup_errors', 1);
+error_reporting(E_ALL);
+
+$aProducto = array();
+$aProducto[] = array("nombre" => "Smart tv 55\" 4K UHD",
+    "marca" => "Hitachi",
+    "modelo" => "554KS20",
+    "stock" => 60,
+    "precio" => 58000
+);
+$aProducto[] = array("nombre" => "Samsung Galaxy A30 Blanco",
+    "marca" => "Samsung",
+    "modelo" => "Galaxy A30",
+    "stock" => 0,
+    "precio" => 22000
+);
+$aProducto[] = array("nombre" => "Aire acondicionado Split Inverter frio/calor Surrey 2900F",
+    "marca" => "Surrey",
+    "modelo" => "5543299585AS",
+    "stock" => 5,
+    "precio" => 45000
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de productos</title>
+</head>
+<body>
+    
+</body>
+</html>
