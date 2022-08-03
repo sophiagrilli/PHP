bd666cf75b8738a70510759e0f9d7993d2c48ebb
sophiagrilli <sophiagrilli@yahoo.com>
2022-08-03 05:17:07 -0300

    cambios

--- listado_productos.php
+++ listado_productos.php
@@ -4,19 +4,22 @@ ini_set('display_statup_errors', 1);
 error_reporting(E_ALL);
 
 $aProducto = array();
-$aProducto[] = array("nombre" => "Smart tv 55\" 4K UHD",
+$aProducto[] = array(
+    "nombre" => "Smart tv 55\" 4K UHD",
     "marca" => "Hitachi",
     "modelo" => "554KS20",
     "stock" => 60,
     "precio" => 58000
 );
-$aProducto[] = array("nombre" => "Samsung Galaxy A30 Blanco",
+$aProducto[] = array(
+    "nombre" => "Samsung Galaxy A30 Blanco",
     "marca" => "Samsung",
     "modelo" => "Galaxy A30",
     "stock" => 0,
     "precio" => 22000
 );
-$aProducto[] = array("nombre" => "Aire acondicionado Split Inverter frio/calor Surrey 2900F",
+$aProducto[] = array(
+    "nombre" => "Aire acondicionado Split Inverter frio/calor Surrey 2900F",
     "marca" => "Surrey",
     "modelo" => "5543299585AS",
     "stock" => 5,
@@ -27,13 +30,18 @@ $aProducto[] = array("nombre" => "Aire acondicionado Split Inverter frio/calor S
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de productos</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
